e929ed65168324400da9322e0cf98dd3de8c12a2
kzhelev <117535216+kzhelev@users.noreply.github.com>
2023-05-11 14:41:58 +0300

    Initial commit

--- ObjectsAndClasses/Exc6/Main.java
+++
@@ -1,7 +0,0 @@
-package ObjectsAndClasses.Exc6;
-
-public class Main {
-    public static void main(String[] args) {
-        AdvertisementMessage.messageGenerator();
-    }
-}
--- ObjectsAndClasses/Exc9/Main.java
+++
@@ -1,7 +0,0 @@
-package ObjectsAndClasses.Exc9;
-
-public class Main {
-    public static void main(String[] args) {
-        StudentsAndGrades.sortAndPrint();
-    }
-}
